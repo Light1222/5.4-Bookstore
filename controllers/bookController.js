827f2707c498cb63df37c422825cea84788b365c
r7qb <r7qb@fresnou.org>
2023-03-02 11:38:33 -0800

    3rd

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const {Book} = require('../models')
+const genres = ['Action', 'Adventure', 'Comic', 'Comedy', 'Romance', 'Fantasy', 'Fiction', 'Non-Fiction'].sort();
 
 //view all
 module.exports.viewAll = async function (req, res) {
@@ -7,15 +8,56 @@ module.exports.viewAll = async function (req, res) {
 }
 //profile
 module.exports.viewProfile= async function(req, res) {
-    const course = await Book.findByPk(req.params.id);
-    res.render('book/profile', {Book})
+    const book = await Book.findByPk(req.params.id);
+    res.render('book/profile', {book})
 }
 //render add form
+module.exports.renderAddForm = function(req, res){
+    const book = {
+        title: '',
+        authors: '',
+        publisher: '',
+        genre: 0,
+        pages: '',
+        image: '',
+        description: '',
 
+    }
+    res.render('book/add', {book, genres});
+}
 //add
-
+module.exports.addBook = async function(req, res){
+    const book = await Book.create({
+        title: req.body.title,
+        authors: req.body.authors,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pages: req.body.pages,
+        image: req.body.image,
+        description: req.body.description,
+    });
+    res.redirect(`/books/profile/${book.id}`);
+}
 //render edit form
-
+module.exports.renderEditForm = async function(req, res){
+    const book = await Book.findByPk(req.params.id);
+    res.render('book/edit', {book, genres});
+}
 //update
-
+module.exports.updateBook = async function(req, res){
+    const book = await Book.update({
+        title: req.body.title,
+        authors: req.body.authors,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pages: req.body.pages,
+        image: req.body.image,
+        description: req.body.description,
+        }, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect(`/books/profile/${req.params.id}`)
+}
 //delete
